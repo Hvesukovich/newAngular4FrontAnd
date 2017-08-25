27e11cb806e4b29764c303ef8445333c46d62cd7
Sergei <3Hopkins@mail.ru>
2017-08-25 14:33:56 +0300

    Создан скилет из компонентов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConstructorComponent } from './components/constructor/constructor.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConstructorComponent,
+    HeaderComponent,
+    SidenavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
